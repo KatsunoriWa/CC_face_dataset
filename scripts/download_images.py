462f1a31970410a54777607350087586e9de5d43
papa <gtx285aa@yahoo.co.jp>
2018-12-31 17:00:43 +0900

    renamed output file

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -11,10 +11,11 @@ def download_list(name):
     imageDir = Path("../imageDir") / name.stem
     imageDir.mkdir(parents=True, exist_ok=True)
 
-    for line in open(name, "rt"):
+    for i, line in enumerate(open(name, "rt")):
         if line.find("https") >-1:
             url = line.strip()
-            basename = os.path.basename(url)
+#            basename = os.path.basename(url)
+            basename = "%s_%04d.jpg" % (name.stem, i)
             outname = imageDir/ Path(basename)
             if not outname.is_file():
                 try:
